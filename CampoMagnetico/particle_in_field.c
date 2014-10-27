d9f15c0d33a04cc14748f0f2e47955981d415a23
MendezV <som.felipe66@gmail.com>
2014-10-27 13:24:15 -0500

    correcciones en velocidad, n_points y k en el campo magnético

--- CampoMagnetico/particle_in_field.c
+++ CampoMagnetico/particle_in_field.c
@@ -18,13 +18,13 @@ int main(int argc, char **argv){
     	double xfunc, yfunc, zfunc;
         double xfuncprime, yfuncprime, zfuncprime;
         double E0 ,E0j,alpha,v0, gamma;
-        double Bx,By,Bz,r,k;
+        double Bx,By,Bz,r,k,r2;
     	int j,i,n_points;
     
-        h=0.001;
+        h=1;
     	tmin=0.0;
     	tmax=100.0;
-        n_points=100;
+        n_points=(tmax-tmin)/h;
         t=malloc(n_points*sizeof(double));
         x=malloc(n_points*sizeof(double));
         y=malloc(n_points*sizeof(double));
@@ -37,9 +37,10 @@ int main(int argc, char **argv){
     E0 = atoi(argv[1]);
     E0j = E0*conversion;
     alpha = atoi(argv[2]);
-    v0=sqrt(1-(1/(1+pow((E0/(m*pow(c,2))),2))));
+    v0=c*sqrt(1-(1/pow(1+E0j/(m*pow(c,2)),2)));
     gamma =1+(E0/(m*pow(c,2)));
     k=-(e*B0*pow(Rt,3))/(m*gamma);
+    printf("%f \n",v0);
     
 //abre archivo para escribir
     
@@ -64,22 +65,23 @@ int main(int argc, char **argv){
 
 //condiciones iniciales
     t[0]=0.0;
-    x[0]=Rt;
+    x[0]=2*Rt;
     y[0]=0.0;
     z[0]=0.0;
     xprime[0]=0;
     yprime[0]=v0*cos(alpha* PI / 180.0);
     zprime[0]=v0*sin(alpha * PI / 180.0);
     
-    fprintf(in, "%f\t%f\t%f\t%f \n",t[0],x[0],y[0],z[0]);
+  fprintf(in, "%f\t%f\t%f\t%f\t%f\t%f\t%f \n",t[0],x[0],y[0],z[0],xprime[0],yprime[0],zprime[0]);
    
 //primer paso de euler
     
     r=pow(x[0],2)+pow(y[0],2)+pow(z[0],2);
+    r2=pow(r,0.5);
     
-    Bx=(3*y[0]*x[0])/pow(r,5);
-    By=(3*y[0]*z[0])/pow(r,5);
-    Bz=k*(2*pow(z[0],2)-pow(y[0],2)-pow(x[0],2))/pow(r,5);
+    Bx=k*(3*y[0]*x[0])/pow(r2,5);
+    By=k*(3*y[0]*z[0])/pow(r2,5);
+    Bz=k*(2*pow(z[0],2)-pow(y[0],2)-pow(x[0],2))/pow(r2,5);
               
 
     xfunc=Bz*yprime[0]-By*zprime[0];
@@ -90,24 +92,26 @@ int main(int argc, char **argv){
     zfuncprime=zprime[0];
     
     t[1]=t[0]+h;
+    xprime[1]=xprime[0]+h*xfunc;
+    yprime[1]=yprime[0]+h*yfunc;
+    zprime[1]=zprime[0]+h*zfunc;
     x[1]=x[0]+h*xfuncprime;
     y[1]=y[0]+h*yfuncprime;
     z[1]=z[0]+h*zfuncprime;
-    xprime[1]=xprime[0]+h*xfuncprime;
-    yprime[1]=yprime[0]+h*yfuncprime;
-    zprime[1]=zprime[0]+h*zfuncprime;
     
-    fprintf(in, "%f\t%f\t%f\t%f \n",t[1],x[1],y[1],z[1]);
+    
+    fprintf(in, "%f\t%f\t%f\t%f\t%f\t%f\t%f \n",t[1],x[1],y[1],z[1],xprime[1],yprime[1],zprime[1]);
     
     //leapfrog
     
     for(i=1;i<(n_points-1);i++){
         
         r=pow(x[i-1],2)+pow(y[i-1],2)+pow(z[i-1],2);
+        r2=pow(r,0.5);
         
-        Bx=(3*y[i-1]*x[i-1])/pow(r,5);
-        By=(3*y[i-1]*z[i-1])/pow(r,5);
-        Bz=k*(2*pow(z[i-1],2)-pow(y[i-1],2)-pow(x[i-1],2))/pow(r,5);
+        Bx=k*(3*y[i-1]*x[i-1])/pow(r2,5);
+        By=k*(3*y[i-1]*z[i-1])/pow(r2,5);
+        Bz=k*(2*pow(z[i-1],2)-pow(y[i-1],2)-pow(x[i-1],2))/pow(r2,5);
         
         
         xfunc=Bz*yprime[i-1]-By*zprime[i-1];
@@ -118,15 +122,16 @@ int main(int argc, char **argv){
         zfuncprime=zprime[i-1];
         
         t[i+1]=t[i-1]+2*h;
+        xprime[i+1]=xprime[i-1]+2*h*xfunc;
+        yprime[i+1]=yprime[i-1]+2*h*yfunc;
+        zprime[i+1]=zprime[i-1]+2*h*zfunc;
         x[i+1]=x[i-1]+2*h*xfuncprime;
         y[i+1]=y[i-1]+2*h*yfuncprime;
         z[i+1]=z[i-1]+2*h*zfuncprime;
-        xprime[i+1]=xprime[i-1]+2*h*xfuncprime;
-        yprime[i+1]=yprime[i-1]+2*h*yfuncprime;
-        zprime[i+1]=zprime[i-1]+2*h*zfuncprime;
         
         
-         fprintf(in, "%f\t%f\t%f\t%f \n",t[i+1],x[i+1],y[i+1],z[i+1]);
+        
+         fprintf(in, "%f\t%f\t%f\t%f\t%f\t%f\t%f \n",t[i+1],x[i+1],y[i+1],z[i+1],xprime[i+1],yprime[i+1],zprime[i+1]);
     }
     
 
