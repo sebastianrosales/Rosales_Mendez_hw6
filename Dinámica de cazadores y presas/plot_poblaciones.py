68a4c1a1cb8a1c1301deca04689e87677f028cb1
MendezV <som.felipe66@gmail.com>
2014-10-26 21:26:02 -0500

    poblaciones resuelto

---
+++ Dinámica de cazadores y presas/plot_poblaciones.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) != 2:
+    print "Este programa necesita un (1) argumento para funcionar"
+    sys.exit()
+
+
+filename=sys.argv[1]
+pimp=np.loadtxt(filename)
+
+n_points = 1000
+x = pimp[:,1]
+y  =  pimp[:,2]
+
+fig = plt.figure(figsize=(9.5,9.0))
+ax = plt.axes()
+
+ax.set_xlabel("$x$",fontsize=25)
+ax.set_ylabel("$y$",fontsize=25)
+ax.set_title("$\mathrm{Population\ growth}$", fontsize=25)
+
+
+plt.plot(x,y, color='red', label="$f(x)=y$")
+
+ax.legend()
+
+
+filename = filename[:-4]
+plt.savefig(filename + '.pdf',format = 'pdf', transparent=True)
+plt.close()
